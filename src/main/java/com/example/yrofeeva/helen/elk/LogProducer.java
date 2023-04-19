2ae10b3469c80db6732690f09e65caba64d315e4
Helen Yrofeeva <helen_yrofeeva@epam.com>
2023-04-19 16:29:30 +0300

    upgrade spring, control elastic access from app via config

--- src/main/java/com/example/yrofeeva/helen/elk/LogProducer.java
+++ src/main/java/com/example/yrofeeva/helen/elk/LogProducer.java
@@ -5,9 +5,12 @@ import com.example.yrofeeva.helen.elk.model.Event;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -17,6 +20,12 @@ import java.util.concurrent.TimeUnit;
 public class LogProducer implements CommandLineRunner {
     @Autowired
     ElasticClient lowLevelElasticClient;
+    @Value("${produce.logs}")
+    private boolean produceLogs;
+    @Value("${elastic.client.enabled}")
+    private boolean elasticClientEnabled;
+
+    private Random random = new Random();
 
     private static Event buildEvent(String id, String title) {
         var event = new Event();
@@ -33,24 +42,42 @@ public class LogProducer implements CommandLineRunner {
     private int getRandom() {
         int min = 1;
         int max = 10;
-        Random random = new Random();
         return random.nextInt(max - min) + min;
+    }
+
+    private void updateEvents() {
+        for (int i = 1; i < 5; i++) {
+            lowLevelElasticClient.updateEvent(buildEvent("" + i, "event " + i));
+        }
     }
 
     @SneakyThrows
     @Override
-    public void run(String... args) {
-        for (int i = 1; i < 5; i++) {
-            lowLevelElasticClient.updateEvent(buildEvent("" + i, "event " + i));
+    public void run(String... args) throws Exception {
+        log.info("Console app run -- start");
+        log.info("produce.logs: {}", produceLogs);
+        log.info("elastic.client.enabled: {}", elasticClientEnabled);
+
+        var produceLogsLocal = produceLogs;
+
+        if (elasticClientEnabled) {
+            updateEvents();
         }
-        while (true){
+
+        var startTime = LocalDateTime.now();
+        while (produceLogsLocal){
             var randomId = getRandom();
             log.info("GET Event by id: {}", randomId);
-            var event = lowLevelElasticClient.getEvent(""+randomId);
-            if(event.isPresent()){
-                log.info("Event: " + event.get().getTitle());
+
+            if(elasticClientEnabled) {
+                var event = lowLevelElasticClient.getEvent("" + randomId);
+                event.ifPresent(value -> log.info("Event: " + value.getTitle()));
             }
-            TimeUnit.SECONDS.sleep(2);
+
+            TimeUnit.SECONDS.sleep(5);
+            produceLogsLocal = ChronoUnit.MINUTES.between(startTime, LocalDateTime.now()) < 30;
         }
+        log.info("Console app run -- end");
+        System.exit(1);
     }
 }
